bcf1ad0c1026ba963eefd91a6f7fc933e941193e
Nadezhda <nbykova79@gmail.com>
2022-07-02 11:35:44 +0300

    определяем вторую цифру в трехзначном числе

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// выдает вторую цифру из введенного трёхзначного число
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+
+string SecondNum(int num1)
+{
+if (num1 <= 99 || num1 > 999) 
+{
+return "Вы ввели НЕ трехзначное число";
+}
+else
+return $"Вторая цифра в числе {num1%100/10-num1%10/10}";
+}
+Console.WriteLine(SecondNum(num));
